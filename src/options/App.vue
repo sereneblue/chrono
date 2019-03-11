fc90da27435fcb602713f7559087ec13885cb3fc
sereneblue <sereneblue@users.noreply.github.com>
2019-03-10 23:11:09 -0400

    add tabs

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>
-    <p>Hello world!</p>
-  </div>
+  <v-app dark>
+    <v-tabs
+      centered
+      color="indigo"
+      dark
+      slider-color="white"
+    >
+      <v-tab :key="history">
+        <v-icon left small>history</v-icon>
+        History
+      </v-tab>
+
+      <v-tab :key="stats">
+        <v-icon left small>trending_up</v-icon>
+        Stats
+      </v-tab>
+
+      <v-tab :key="settings">
+        <v-icon left small>settings</v-icon>
+        Settings
+      </v-tab>
+
+      <v-tab-item :key="history">
+        <v-container fluid>
+          1
+        </v-container>
+      </v-tab-item>
+
+      <v-tab-item :key="stats">
+        <v-container fluid>
+          2
+        </v-container>
+      </v-tab-item>
+
+      <v-tab-item :key="settings">
+        <v-container fluid>
+          3
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-app>
 </template>
 
 <script>
@@ -11,7 +49,4 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 20px;
-}
 </style>
