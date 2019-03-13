b3f9aeb324626ec9fd9c97d0624b1c74cd89d431
sereneblue <sereneblue@users.noreply.github.com>
2019-03-12 23:41:43 -0400

    fix component binding

--- src/options/App.vue
+++ src/options/App.vue
@@ -6,34 +6,34 @@
       dark
       slider-color="white"
     >
-      <v-tab :key="history">
+      <v-tab key="history">
         <v-icon left small>history</v-icon>
         History
       </v-tab>
 
-      <v-tab :key="stats">
+      <v-tab key="stats">
         <v-icon left small>trending_up</v-icon>
         Stats
       </v-tab>
 
-      <v-tab :key="settings">
+      <v-tab key="settings">
         <v-icon left small>settings</v-icon>
         Settings
       </v-tab>
 
-      <v-tab-item :key="history">
+      <v-tab-item key="history">
         <v-container fluid>
           1
         </v-container>
       </v-tab-item>
 
-      <v-tab-item :key="stats">
+      <v-tab-item key="stats">
         <v-container fluid>
           2
         </v-container>
       </v-tab-item>
 
-      <v-tab-item :key="settings">
+      <v-tab-item key="settings">
         <v-container fluid>
           3
         </v-container>
