0d50ea8aa776b106a1e80506e213cd3179b1309c
sereneblue <sereneblue@users.noreply.github.com>
2019-03-16 20:44:29 -0400

    update stats view

--- src/options/stats/StatsView.vue
+++ src/options/stats/StatsView.vue
@@ -1,11 +1,63 @@
 <template>
+  <v-container fluid grid-list-md text-lg-center>
+    <v-layout row wrap>
+      <v-flex lg4>
+        <v-card>
+          <v-list subheader>
+            <v-subheader>Top Domains</v-subheader>
+            <v-list-tile
+              v-for="domain in topDomains"
+              :key="domain[0]"
+            >
+              <v-list-tile-content>
+                <v-list-tile-title>{{ domain[0] }} </v-list-tile-title>
+              </v-list-tile-content>
+
+              <v-list-tile-action>
+                <v-chip label>{{ domain[1] }} visits</v-chip>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+      <v-flex lg8>
+        <v-card min-height="100%">
+          <v-subheader>Browsing History Trend</v-subheader>
+          <VisitsTrend :data="visits"/>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <v-card min-height="100%">
+          <v-subheader>Day of Week</v-subheader>
+          <DayOfWeekTrend :data="dayOfWeek" />
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card>
+          <v-subheader>Time of Day</v-subheader>
+          <TimeOfDayTrend :data="timeOfDay" />
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import VisitsTrend from './components/VisitsTrend.vue';
+import DayOfWeekTrend from './components/DayOfWeekTrend.vue';
+import TimeOfDayTrend from './components/TimeOfDayTrend.vue';
+
 var moment = require('moment');
 
 export default {
   name: 'StatsView',
+  components: {
+    VisitsTrend,
+    DayOfWeekTrend,
+    TimeOfDayTrend
+  },
   async created() {
     await this.calculateStats(90);
   },
