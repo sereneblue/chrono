5cf65b7f83568c405eb85172e158feff305c7ea2
sereneblue <sereneblue@users.noreply.github.com>
2019-03-29 21:27:53 -0400

    v1.0.1; fix icon issue on addon page

--- src/options/settings/SettingsView.vue
+++ src/options/settings/SettingsView.vue
@@ -1,12 +1,6 @@
 <template>
   <v-container fluid grid-list-md text-lg-center>
-    <v-select
-      :color="themeColor"
-      :items="tabs"
-      :value="currentTab"
-      @change="updateMainTab"
-      label="Main tab"
-    >
+    <v-select :color="themeColor" :items="tabs" :value="currentTab" @change="updateMainTab" label="Main tab">
       <template slot="item" slot-scope="data">
         {{ data.item | toTitleCase }}
       </template>
@@ -15,13 +9,7 @@
         {{ data.item | toTitleCase }}
       </template>
     </v-select>
-    <v-select
-      :color="themeColor"
-      :items="colorsList"
-      :value="currentTheme"
-      @change="changeThemeColor"
-      label="Theme color"
-    >
+    <v-select :color="themeColor" :items="colorsList" :value="currentTheme" @change="changeThemeColor" label="Theme color">
       <template slot="item" slot-scope="data">
         <v-icon large :color="getBaseColor(data.item)">label</v-icon>&nbsp;&nbsp;{{ data.item | toTitleCase }}
       </template>
@@ -30,35 +18,24 @@
         {{ data.item | toTitleCase }}
       </template>
     </v-select>
-    <v-switch
-      v-model="darkModeEnabled"
-      :color="themeColor"
-      :label="`Dark Mode ${darkModeEnabled ? 'Enabled' : 'Disabled'}`"
-    ></v-switch>
-    <div class="title font-weight-medium">
-      chrono v1.0.0 by <v-btn small href="https://github.com/sereneblue" target="_blank">sereneblue</v-btn>
-    </div>
+    <v-switch v-model="darkModeEnabled" :color="themeColor" :label="`Dark Mode ${darkModeEnabled ? 'Enabled' : 'Disabled'}`"></v-switch>
+    <div class="title font-weight-medium">chrono v1.0.1 by <v-btn small href="https://github.com/sereneblue" target="_blank">sereneblue</v-btn></div>
   </v-container>
 </template>
 
 <script>
-export default {  
+export default {
   name: 'SettingsView',
   data() {
     return {
-      tabs: [
-        'history',
-        'search',
-        'stats'
-      ]
-    }
+      tabs: ['history', 'search', 'stats'],
+    };
   },
   computed: {
     colorsList() {
-      return Object
-        .keys(this.$colors)
+      return Object.keys(this.$colors)
         .slice(0, -1)
-        .map(c => c.replace(/([A-Z])/g, (match) => `-${match.toLowerCase()}`));
+        .map(c => c.replace(/([A-Z])/g, match => `-${match.toLowerCase()}`));
     },
     currentTab() {
       return this.$store.state.mainTab;
@@ -67,34 +44,34 @@ export default {
       return this.$store.state.themeColor;
     },
     darkModeEnabled: {
-      get () {
+      get() {
         return this.$store.state.darkModeEnabled;
       },
-      set (value) {
+      set(value) {
         this.$store.dispatch('toggleDarkMode', value);
-      }
+      },
     },
     themeColor() {
       return this.$store.state.themeColor;
-    }
+    },
   },
   filters: {
     toTitleCase(text) {
-      if (!text.includes("-")) {
+      if (!text.includes('-')) {
         return `${text.charAt(0).toUpperCase() + text.slice(1)}`;
       }
 
       let parts = text.split('-');
       return `${parts[0].charAt(0).toUpperCase() + parts[0].slice(1)} ${parts[1].charAt(0).toUpperCase() + parts[1].slice(1)}`;
-    } 
+    },
   },
   methods: {
     changeThemeColor(theme) {
       this.$store.dispatch('changeTheme', theme);
     },
     getBaseColor(c) {
-      if (c.includes("-")) {
-        let parts = c.split('-'); 
+      if (c.includes('-')) {
+        let parts = c.split('-');
         c = `${parts[0]}${parts[1].charAt(0).toUpperCase() + parts[1].slice(1)}`;
       }
 
@@ -102,7 +79,7 @@ export default {
     },
     updateMainTab(tab) {
       this.$store.dispatch('updateMainTab', tab);
-    }
-  }
+    },
+  },
 };
 </script>
