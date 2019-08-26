6aa0eb6e76607d8e99093ebae85d0868319818b5
sereneblue <sereneblue@users.noreply.github.com>
2019-08-25 21:03:22 -0400

    update vuetify

--- src/options/options.js
+++ src/options/options.js
@@ -1,17 +1,14 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
 import Vue from 'vue';
 import App from './App';
 import Chart from 'chart.js';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import vuetify from '../plugins/vuetify';
 import 'typeface-roboto';
 
 import colors from 'vuetify/es5/util/colors';
-import moment from 'moment'
+import moment from 'moment';
 import store from '../store';
 
-Vue.prototype.$copy = (text) => {
+Vue.prototype.$copy = text => {
   const el = document.createElement('textarea');
   el.value = text;
   document.body.appendChild(el);
@@ -22,11 +19,10 @@ Vue.prototype.$copy = (text) => {
 Vue.prototype.$colors = colors;
 Vue.prototype.$moment = moment;
 
-Vue.use(Vuetify);
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App),
-  store
+  store,
+  vuetify,
 });
