bc44689134d61b0c238226f3c7b6ea249d5a8878
sereneblue <sereneblue@users.noreply.github.com>
2019-08-26 22:26:59 -0400

    update icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,4 +1,4 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
